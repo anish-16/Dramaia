5120a63bcc74e44e8e75c0c3c8ffd36ff8e8f174
Anish Kumar <akanish1607@gmail.com>
2024-07-11 01:18:46 +0530

    Create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,36 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+        <h1 className='text-2xl font-medium text-amber-600'>ABOUT</h1>
+        <p>
+            Welcome to our movie database website! We are a team of passionate
+            movie enthusiasts who have come together to create a one-stop destination for
+            all your movie-related needs.
+        </p>
+
+        <p>
+            Our website is designed to provide you with a comphrensive database of
+            movies from all around the world, along with the latest news, reviews,
+            and trailers. Our movies database is constantly updated with new 
+            releases, ensuring that you have access to the latest and greatest in 
+            the world of cinema. you can search for movies by title, director, actor, 
+            genre, or release date, making it easy to find the perfect moive 
+            for any occassion .
+             </p>
+
+        <p>
+             In addition to our extensive movie database, We also offer a platform 
+             for movie lovers to connect and share their thoughts on the latest releases.
+             Our community section includes a forum where you can discuss your favourite 
+              movies with like-minded individuals and read reviews and ratings from other
+             users. We also have a section dedicated to movies news and trailers. keeping
+             you up to date with the latest developments in the world of cinema. thank you 
+             for visiting our website, we hope you enjoy exploring our extensive movie database.
+             if you have any feedback or suggestion, please feel free to contact us. We are 
+             always looking for ways to improve and inhance the user experience on our website.
+             Happy browsing!
+
+        </p>
+    </div>
+  );
+}
