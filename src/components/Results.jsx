0aa44baa5eca6f96ce8de70b824e5a46823997a9
Anish Kumar <akanish1607@gmail.com>
2024-07-21 22:29:41 +0530

    Fetch data from TMDB api in the home page

---
+++ src/components/Results.jsx
@@ -0,0 +1,16 @@
+ import React from 'react'
+ 
+ export default function Results({results}) {
+   return (
+     <div>
+        {
+            results.map((result, index) => (
+                <div key={results.id}>
+                    <h2>{result.original_title}</h2>
+                    </div>
+            ))
+        }
+     </div>
+   );
+ }
+ 
